ecc45eccb05a0bf4cd147e5767822961261cd50d
quoituparle <daiyue.han@outlook.com>
2026-03-22 17:15:58 +0800

    LC-two-sum-ii-input-array-is-sorted [Runtime: 4 ms; Memory: 19.6 MB]

---
+++ array/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int start = 0;
+        int end = n - 1;
+        
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum == target) {
+                return {start+1, end+1};
+            };
+            if (sum > target) end--;
+            if (sum < target) start++;
+        }
+        return {};
+    }
+};
