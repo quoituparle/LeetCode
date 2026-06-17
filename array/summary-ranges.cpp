bf03b6a988e44dac287407569ee8d55935d4658e
quoituparle <daiyue.han@outlook.com>
2026-06-17 23:58:35 +0800

    LC-summary-ranges [Runtime: N/A; Memory: N/A]

---
+++ array/summary-ranges.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return {};
+        int s = 0;
+        vector<string> ans;
+        for (int i = 0; i < n; ++i) {
+            if ((nums[i] - nums[s] != i - s)) {
+                ans.emplace_back(output(nums[s], nums[i-1]));
+                s = i;
+            }
+        }
+        ans.emplace_back(output(nums[s], nums[n-1]));
+        return ans;
+    }
+private:
+    string output(int a, int b) {
+        if (a == b) return to_string(a);
+        string output = to_string(a) + "->" + to_string(b);
+        return output;
+    }
+
+    string output(int a) {
+        return to_string(a);
+    }
+};
